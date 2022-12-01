16ddbc4af365910c7b92a5b6239da35df2aac9c1
Yassine <johndoe@example.com>
2022-12-01 14:11:26 +0100

    Raadeens

--- Module 03/Proef A/M03.PA-Raadeens/raadeens.py
+++ Module 03/Proef A/M03.PA-Raadeens/raadeens.py
@@ -1,19 +1,16 @@
 import random
 score = 0
-rondes = 0
+ronde = 0
 te_raden_getal = random.randint(1,1000)
-geraden_getal = 0
 print(te_raden_getal)
-for stop in range(20):
-    vraag = input("wil je een spel spelen (ja/nee)").lower()
-    if vraag != "ja":
-        break
-    for x in range(10):
+while ronde <20 :
+    teller = 0
+    while teller != 10:
         geraden_getal = int(input("Raad een getal tussen 1 en 1000:"))
         if geraden_getal == te_raden_getal:
             print("geraden")
             score +=1
-            break
+            teller = 10
         verschilsgetal = abs(te_raden_getal - geraden_getal)
         if te_raden_getal > geraden_getal:
             print("Hoger")
@@ -22,11 +19,13 @@ for stop in range(20):
         if verschilsgetal <=20:
             print("je bent heel warm")
         elif verschilsgetal <=50:
-            print("Je bent  warm")               
-    ronde_vraag = input('nog een getal raden?')
-    if ronde_vraag != 'ja':
-        break
-print(f"je hebt {score} keer het getal geraden")           
+            print("Je bent  warm")
+    ronde +=1
+    if ronde < 20:
+        stopvraag = input("wil je nog verder spelen ja of nee")
+        if stopvraag == "nee":  
+            ronde= 20      
+    print(f"je hebt {score} keer het getal geraden")           
 # stap 1: raad 1 getal
 # stap 2: 3 controles
 # stap 3: programeer 1 ronde
