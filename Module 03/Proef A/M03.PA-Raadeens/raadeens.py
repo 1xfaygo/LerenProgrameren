7c1445179d56cb32c5df1ef0c5243ca0949af69e
Yassine <johndoe@example.com>
2022-12-05 13:19:41 +0100

    raadeens

--- Module 03/Proef A/M03.PA-Raadeens/raadeens.py
+++ Module 03/Proef A/M03.PA-Raadeens/raadeens.py
@@ -1,17 +1,18 @@
 import random
 score = 0
 ronde = 0
-te_raden_getal = random.randint(1,1000)
-print(te_raden_getal)
 while ronde <20 :
+    te_raden_getal = random.randint(1,1000)
+    print(te_raden_getal)
     teller = 0
-    while teller != 10:
+    while teller < 10:
         geraden_getal = int(input("Raad een getal tussen 1 en 1000:"))
         if geraden_getal == te_raden_getal:
             print("geraden")
             score +=1
             teller = 10
         verschilsgetal = abs(te_raden_getal - geraden_getal)
+        teller+=1# dit doe ik omdat ik wil dat de while loop stopt bij 10 keer raden
         if te_raden_getal > geraden_getal:
             print("Hoger")
         elif te_raden_getal < geraden_getal:
@@ -26,9 +27,4 @@ while ronde <20 :
         if stopvraag == "nee":  
             ronde= 20      
     print(f"je hebt {score} keer het getal geraden")           
-# stap 1: raad 1 getal
-# stap 2: 3 controles
-# stap 3: programeer 1 ronde
-# stap 4: programeer 20 roden
-# stap 5: input maken
-# stap 6: teller maken met score
+    
