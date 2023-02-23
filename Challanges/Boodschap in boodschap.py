25fc9960b9b81774118ff1c6528cf550db69faa1
Yassine <johndoe@example.com>
2023-02-23 10:30:15 +0100

    opdracht

---
+++ Challanges/Boodschap in boodschap.py
@@ -0,0 +1,16 @@
+def tussen_haakjes(tekst):
+    resultaat = ""
+    tussen_haakjes = False
+    for teken in tekst:
+        if teken == "[":
+            tussen_haakjes = True
+        elif teken == "]":
+            tussen_haakjes = False
+        elif tussen_haakjes:
+            resultaat += teken
+    return resultaat
+    
+tekst = """En ze stu[re]n [i]ngekleurde prentbriefkaarten van plekken waarvan ze zich niet reali[s]eren dat ze er nooit geweest zijn [a]an ' Iedereen op nummer 22, weer is prachti[g], onz[e] kamer is aa[n]gekruisd. Missen jullie. E[t]en[ ]i[s] vettig , maar we hebben een geweldig leuk restaurantje gevonden in de achterstraatjes waar ze Heine[ke]n hebben en kaas en uien chips en iemand die "Een beetje verliefd" speel[t] op een a[c]cordeon ' en je zit vier dagen vast op Schip[h]ol voor je vijfdaagse vliegvakantie met niks anders te eten dan uitgedroogde voorverpakte boterhammen..."""
+
+resultaat = tussen_haakjes(tekst)
+print(resultaat)
