67eaf9c03bcfaf9aef6c46ee3ea988a43997afda
Yassine Oufkir <1xfaygo@gmail.com>
2023-11-01 07:03:16 +0100

    papi

--- Module 05/Proef A - Een DRY ijssalon/Papi_gelato.py
+++ Module 05/Proef A - Een DRY ijssalon/Papi_gelato.py
@@ -1,11 +1,11 @@
 from functions import aantal_bolletjes
-from functions import aantal_bolletjes_check
+from functions import main
 from functions import bakje_horentje
 from functions import opnieuw_bestellen
 
 
 print("Welkom bij papi gelato je mag alle smaken kiezen zolang het maar vanille ijs is.")
 aantal_bolletjes()
-aantal_bolletjes_check()
+main()
 bakje_horentje()
 opnieuw_bestellen()
--- Module 05/Proef A - Een DRY ijssalon/functions.py
+++ Module 05/Proef A - Een DRY ijssalon/functions.py
@@ -1,36 +1,71 @@
+# def aantal_bolletjes():
+#     while True:
+#         try:
+#             aantal = int(input('Hoeveel bolletjes wilt u?: '))
+#             return aantal
+#         except ValueError:
+#             print('Sorry dat begrijp ik niet. Voer een geldig getal in.')
+
+# def aantal_bolletjes_check():
+#     aantal = aantal_bolletjes()
+#     if aantal <= 4:
+#         bakje_of_horentje = bakje_horentje()
+#     elif 4 <= aantal <= 8:
+#         print(f'Hier u bakje met {aantal} ijsbollen.')
+#         opnieuw_bestellen()
+#     else:
+#         print('Sorry dat hebben wij niet.')
+    
+# def bakje_horentje():
+#     bakje_vraag = input('Wilt u een bakje of hoorentje?:')
+    
+#     if bakje_horentje != 'bakje':
+#         return 'horentje'
+#     elif bakje_horentje == 'bakje':
+#         return 'bakje'
+#     else:
+#         print('Sorry dat begrijp ik niet.')
+#     return bakje_horentje() 
+
+# def opnieuw_bestellen():
+#     while True:
+#             opnieuw_bestellen_vraag = input('wilt u opnieuw bestellen?N (JA/NEE):')
+#             if opnieuw_bestellen_vraag != 'ja':
+#                     print('Dankuwel voor bestellen bij papi gelato.')
+#                     return opnieuw_bestellen_vraag
 def aantal_bolletjes():
     while True:
         try:
             aantal = int(input('Hoeveel bolletjes wilt u?: '))
             return aantal
         except ValueError:
-            print('Sorry dat begrijp ik niet. Voer een geldig getal in.')
+            print('Sorry, dat begrijp ik niet. Voer een geldig getal in.')
 
-def aantal_bolletjes_check():
-    aantal = aantal_bolletjes()
-    if aantal <= 4:
-        bakje_of_horentje = bakje_horentje()
-    elif 4 <= aantal <= 8:
-        print(f'Hier u bakje met {aantal} ijsbollen.')
-        opnieuw_bestellen()
-    else:
-        print('Sorry dat hebben wij niet.')
-    
 def bakje_horentje():
-    bakje_vraag = input('Wilt u een bakje of hoorentje?:')
-    
-    if bakje_horentje != 'bakje':
-        return 'horentje'
-    elif bakje_horentje == 'bakje':
-        return 'bakje'
-    else:
-        print('Sorry dat begrijp ik niet.')
-    return bakje_horentje() 
+    while True:
+        bakje_vraag = input('Wilt u een bakje of horentje?: ')
+        if bakje_vraag.lower() == 'bakje':
+            return 'bakje'
+        elif bakje_vraag.lower() == 'horentje':
+            return 'horentje'
+        else:
+            print('Sorry, dat begrijp ik niet.')
 
 def opnieuw_bestellen():
-    opnieuw_bestellen_vraag = input('wilt u opnieuw bestellen?:')
+    while True:
+        opnieuw_bestellen_vraag = input('Wilt u opnieuw bestellen? (Ja/Nee): ')
+        if opnieuw_bestellen_vraag.lower() != 'ja':
+            print('Dank u wel voor bestellen bij Papi Gelato.')
+            break
+
+def main():
+    aantal = aantal_bolletjes()
     
-    if opnieuw_bestellen_vraag != 'ja':
-        print('Dankuwel voor bestellen bij papi gelato.')
+    if aantal <= 4:
+        bakje_of_horentje = bakje_horentje()
+        print(f'Hier is uw {bakje_of_horentje} met {aantal} ijsbollen.')
+    elif 4 < aantal <= 8:
+        print(f'Hier is uw bakje met {aantal} ijsbollen.')
+        opnieuw_bestellen()
     else:
-        print("opniew")
+        print('Sorry, dat hebben wij niet.')
